a35f7f05bc583942b0517f12c3e0073f69b0dc4d
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-03-08 17:32:38 -0600

    wip

--- Haskell/lunchnlearn.hs
+++ Haskell/lunchnlearn.hs
@@ -15,14 +15,20 @@ foo s = length s
 bar :: String -> String -> Int
 bar s t = length s + length t
 
+duplicate :: a -> [a]
+duplicate x = [x,x]
+
+
+
+
 -- Currying
 
--- curried :: Char -> (String -> Bool)
-curried :: Char -> String -> Bool
-curried ch st = elem ch st
+-- combine :: String -> (String -> String)
+combine :: String -> String -> String
+combine s t = s ++ t
 
-uncurried :: (Char, String) -> Bool
-uncurried (ch, st) = elem ch st
+prependFoo :: String -> String
+prependFoo = combine "foo"
 
 makePoint :: Int -> Int -> Int -> (Int,Int,Int)
 makePoint x y z = (x,y,z)
@@ -30,13 +36,64 @@ makePoint x y z = (x,y,z)
 -- show :t with partial applications
 -- show that makePoint 1 2 3 = ((makePoint 1) 2) 3
 
+combineUncurried :: (String, String) -> String
+combineUncurried twoStrings = fst twoStrings ++ snd twoStrings
+
 
 -- Higher Order functions
 
 -- show type of map
 
-rev :: [String] -> [String]
-rev xs = map reverse xs
+-- map prependFoo ["bar","baz","bop"]
 
-combine :: String -> String
-combine x y = x ++ y
+-- map (makePoint 1 2) [1..10]
+
+-- map (\x -> makePoint 1 2 x) [1..10]
+
+-- Ask what this will be; show the type of this
+mystery = map (makePoint 1) [1..10]
+
+-- map (\f -> f 1000) mystery
+
+-- filter isUpper ['f', 'F']
+
+
+
+
+-- Pattern Matching, Recursion, Laziness
+-- :t (:)
+-- [1,2,3,4] == 1:2:3:4:[]
+-- "hi" == 'h':'i':[]
+
+-- map toUpper "hello world"
+
+-- there are no loops
+
+map' :: (a -> b) -> [a] -> [b]
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n -1)
+
+-- head (map (+1) [1..999999999999999])
+
+-- [1..]
+ones = 1 : ones
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+
+
+-- Algebraic Data Types
+
+data PizzaTopping = Pepperoni | Cheese | Pineapple
+
+isGood :: PizzaTopping -> Bool
+isGood Pepperoni = True
+isGood Cheese = True
+isGood Pineapple = False
+
+
+
+data Person = Adult String String | Child String
