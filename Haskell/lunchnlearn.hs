ba28cb05f48ea35ffcf348e8681efb1846ce82de
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-03-08 21:55:54 -0600

    add notes

--- Haskell/lunchnlearn.hs
+++ Haskell/lunchnlearn.hs
@@ -1,99 +1,21 @@
-import Data.Char
-
--- Functions
--- immutable, no keywords needed, independent objects, referential transparency
-
-myNum :: Double
-myNum = 10.98
-
-myString :: String
-myString = "paytient"
-
-foo :: String -> Int
-foo s = length s
-
-bar :: String -> String -> Int
-bar s t = length s + length t
-
-duplicate :: a -> [a]
-duplicate x = [x,x]
+-- Functions & Types
 
 
 
-
--- Currying
-
--- combine :: String -> (String -> String)
-combine :: String -> String -> String
-combine s t = s ++ t
-
-prependFoo :: String -> String
-prependFoo = combine "foo"
-
-makePoint :: Int -> Int -> Int -> (Int,Int,Int)
-makePoint x y z = (x,y,z)
-
--- show :t with partial applications
--- show that makePoint 1 2 3 = ((makePoint 1) 2) 3
-
-combineUncurried :: (String, String) -> String
-combineUncurried twoStrings = fst twoStrings ++ snd twoStrings
-
-
--- Higher Order functions
-
--- show type of map
-
--- map prependFoo ["bar","baz","bop"]
-
--- map (makePoint 1 2) [1..10]
-
--- map (\x -> makePoint 1 2 x) [1..10]
-
--- Ask what this will be; show the type of this
-mystery = map (makePoint 1) [1..10]
-
--- map (\f -> f 1000) mystery
-
--- filter isUpper ['f', 'F']
+-- Currying and Partial Application
 
 
 
-
--- Pattern Matching, Recursion, Laziness
--- :t (:)
--- [1,2,3,4] == 1:2:3:4:[]
--- "hi" == 'h':'i':[]
-
--- map toUpper "hello world"
-
--- there are no loops
-
-map' :: (a -> b) -> [a] -> [b]
-map' f [] = []
-map' f (x:xs) = f x : map' f xs
-
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n = n * factorial (n -1)
-
--- head (map (+1) [1..999999999999999])
-
--- [1..]
-ones = 1 : ones
-fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+-- Higher Order Functions
 
 
 
--- Algebraic Data Types
-
-data PizzaTopping = Pepperoni | Cheese | Pineapple
-
-isGood :: PizzaTopping -> Bool
-isGood Pepperoni = True
-isGood Cheese = True
-isGood Pineapple = False
+-- Recursion, Pattern Matching, and Laziness
 
 
 
-data Person = Adult String String | Child String
+-- Algebraic Data Types (time permitting)
+
+
+
+-- Example problem
---
+++ Haskell/lunchnlearnnotes.hs
@@ -0,0 +1,120 @@
+import Data.Char
+
+-- Functions
+-- immutable, no keywords needed, independent objects, referential transparency
+
+myNum :: Double
+myNum = 10.98
+
+myString :: String
+myString = "paytient"
+
+foo :: String -> Int
+foo s = length s
+
+bar :: String -> String -> Int
+bar s t = length s + length t
+
+duplicate :: a -> [a]
+duplicate x = [x,x]
+
+
+
+
+-- Currying
+
+-- combine :: String -> (String -> String)
+combine :: String -> String -> String
+combine s t = s ++ t
+
+prependFoo :: String -> String
+prependFoo = combine "foo"
+
+makePoint :: Int -> Int -> Int -> (Int,Int,Int)
+makePoint x y z = (x,y,z)
+
+-- show :t with partial applications
+-- show that makePoint 1 2 3 = ((makePoint 1) 2) 3
+
+combineUncurried :: (String, String) -> String
+combineUncurried twoStrings = fst twoStrings ++ snd twoStrings
+
+
+-- Higher Order functions
+
+-- show type of map
+
+-- map prependFoo ["bar","baz","bop"]
+
+-- map (makePoint 1 2) [1..10]
+
+-- map (\x -> makePoint 1 2 x) [1..10]
+
+-- Ask what this will be; show the type of this
+mystery = map (makePoint 1) [1..10]
+
+-- map (\f -> f 1000) mystery
+
+-- filter isUpper ['f', 'F']
+
+-- :t (.)
+-- checking multiples of three
+
+
+
+
+-- Pattern Matching, Recursion, Laziness
+-- :t (:)
+-- [1,2,3,4] == 1:2:3:4:[]
+-- "hi" == 'h':'i':[]
+
+-- map toUpper "hello world"
+
+-- there are no loops
+
+map' :: (a -> b) -> [a] -> [b]
+map' f [] = []
+map' f (x:xs) = f x : map' f xs
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n -1)
+
+-- head (map (+1) [1..999999999999999])
+
+-- [1..]
+ones = 1 : ones
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+
+
+-- Algebraic Data Types
+data Bool' = True' | False'
+
+data PizzaTopping = Pepperoni | Cheese | Pineapple
+
+isGood :: PizzaTopping -> Bool
+isGood Pepperoni = True
+isGood Cheese = True
+isGood Pineapple = False
+
+-- data User = Admin String Int
+
+type Point = (Double,Double)
+data Shape = Circle Point Double | Square Point Point
+
+getArea :: Shape -> Double
+getArea (Circle _ radius) = 3.14159265 * radius * radius
+getArea (Square (a,b) (c,d)) = abs (a - c) * abs (b - d)
+
+data Maybe' a = Just' a | Nothing'
+
+safeDivide :: Double -> Double -> Maybe Double
+safeDivide _ 0 = Nothing
+safeDivide x y = Just (x / y)
+
+
+-- also includes records
+-- type and value constructors are themselves functions
+-- tagged union
+
